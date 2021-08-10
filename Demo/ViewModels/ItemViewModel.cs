8a27ab4c5b023cdf7cf7c1773603a7f8e80a9925
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-08-10 09:52:16 +0200

    Initial commit

---
+++ Demo/ViewModels/ItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ReactiveUI;
+
+namespace Demo.ViewModels
+{
+    public class ItemViewModel : ViewModelBase
+    {
+        private string _column1;
+        private string _column2;
+        private string _column3;
+
+        public string Column1
+        {
+            get => _column1;
+            set => this.RaiseAndSetIfChanged(ref _column1, value);
+        }
+
+        public string Column2
+        {
+            get => _column2;
+            set => this.RaiseAndSetIfChanged(ref _column2, value);
+        }
+
+        public string Column3
+        {
+            get => _column3;
+            set => this.RaiseAndSetIfChanged(ref _column3, value);
+        }
+    }
+}
---
+++ Demo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+using ReactiveUI;
+
+namespace Demo.ViewModels
+{
+
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private ObservableCollection<ItemViewModel> _items;
+
+        public ObservableCollection<ItemViewModel> Items
+        {
+            get => _items;
+            set => this.RaiseAndSetIfChanged(ref _items, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            _items = new ObservableCollection<ItemViewModel>()
+            {
+                // new ItemViewModel()
+                // {
+                //     Column1 = "Test 1-1",
+                //     Column2 = "Test 1-2",
+                //     Column3 = "Test 1-3"
+                // },
+                // new ItemViewModel()
+                // {
+                //     Column1 = "Test 2-1",
+                //     Column2 = "Test 2-2",
+                //     Column3 = "Test 2-3"
+                // },
+                // new ItemViewModel()
+                // {
+                //     Column1 = "Test 3-1",
+                //     Column2 = "Test 3-2",
+                //     Column3 = "Test 3-3"
+                // },
+            };
+
+            for (var i = 0; i < 100_000; i++)
+            {
+                var item = new ItemViewModel()
+                {
+                    Column1 = $"Test {i}-1",
+                    Column2 = $"Test {i}-2",
+                    Column3 = $"Test {i}-3"
+                };
+
+                _items.Add(item);
+            }
+        }
+    }
+}
