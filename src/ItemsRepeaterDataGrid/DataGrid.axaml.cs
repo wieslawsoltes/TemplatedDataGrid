cdc394005d643bf391262fc098d0d45f59a75aac
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-08-11 09:32:20 +0200

    Generate template from code

--- src/ItemsRepeaterDataGrid/DataGrid.axaml.cs
+++ src/ItemsRepeaterDataGrid/DataGrid.axaml.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Collections;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
 
 namespace ItemsRepeaterDataGrid
 {
     public class DataGrid : TemplatedControl
     {
+        private Grid? _root;
+        private List<Control> _rootChildren = new List<Control>();
+        private Grid? _columnHeaders;
+        private List<Control> _columnHeadersChildren = new List<Control>();
+        private ScrollViewer? _scrollViewer;
+        private ItemsRepeater? _itemsRepeater;
+
         public static readonly StyledProperty<AvaloniaList<DataGridColumn>> ColumnsProperty = 
             AvaloniaProperty.Register<DataGrid, AvaloniaList<DataGridColumn>>(nameof(Columns), new AvaloniaList<DataGridColumn>());
 
@@ -46,7 +56,194 @@ namespace ItemsRepeaterDataGrid
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
         {
             base.OnApplyTemplate(e);
-            
+
+            _root = e.NameScope.Find<Grid>("PART_Root");
+            _columnHeaders = e.NameScope.Find<Grid>("PART_ColumnHeaders");
+            _scrollViewer = e.NameScope.Find<ScrollViewer>("PART_ScrollViewer");
+            _itemsRepeater = e.NameScope.Find<ItemsRepeater>("PART_ItemsRepeater");
+
+            InvalidateRoot();
+            InvalidateColumnHeaders();
+            InvalidateScrollViewer();
+        }
+
+        protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+        {
+            base.OnPropertyChanged(change);
+
+            if (change.Property == ColumnsProperty)
+            {
+                InvalidateRoot();
+                InvalidateColumnHeaders();
+                InvalidateScrollViewer();
+            }
+        }
+
+        private void InvalidateRoot()
+        {
+            if (_root is null)
+            {
+                return;
+            }
+
+            foreach (var child in _rootChildren)
+            {
+                _root.Children.Remove(child);
+            }
+
+            var columns = Columns;
+ 
+            //  Generate Root RowDefinitions
+
+            var rowDefinitions = new List<RowDefinition>();
+
+            rowDefinitions.Add(new RowDefinition(GridLength.Auto));
+            rowDefinitions.Add(new RowDefinition(new GridLength(1, GridUnitType.Pixel)));
+            rowDefinitions.Add(new RowDefinition(new GridLength(1, GridUnitType.Star)));
+
+            // Generate Root ColumnDefinitions
+
+            var columnDefinitions = new List<ColumnDefinition>();
+            var splitterColumnIndexes = new List<int>();
+
+            for (var i = 0; i < columns.Count; i++)
+            {
+                var column = columns[i];
+
+                var columnDefinition = new ColumnDefinition()
+                {
+                    [!ColumnDefinition.WidthProperty] = column[!!DataGridColumn.WidthProperty]
+                };
+
+                columnDefinitions.Add(columnDefinition);
+
+                if (i < columns.Count - 1)
+                {
+                    columnDefinitions.Add(new ColumnDefinition(new GridLength(1, GridUnitType.Pixel)));
+                    splitterColumnIndexes.Add(columnDefinitions.Count - 1);
+                }
+            }
+
+            _root.SetRowDefinitions(rowDefinitions);
+            _root.SetColumnDefinitions(columnDefinitions);
+
+            // Generate Root Vertical Separator's
+            var horizontalSeparator = new Separator()
+            {
+                Height = 1,
+                [Grid.RowProperty] = 1,
+                [Grid.ColumnProperty] = 0,
+                [Grid.ColumnSpanProperty] = columns.Count + (columns.Count - 1)
+            };
+            _rootChildren.Add(horizontalSeparator);
+
+            // Set ItemsRepeater template
+
+            if (_itemsRepeater is { })
+            {
+                _itemsRepeater.ItemTemplate = new FuncDataTemplate<object>(
+                    (_, _) => new DataGridRow()
+                    {
+                        [!DataGridRow.ColumnsProperty] = this[!DataGrid.ColumnsProperty]
+                    });
+            }
+
+            foreach (var columnIndex in splitterColumnIndexes)
+            {
+                // Generate Root Horizontal Separator's
+                var verticalSeparator = new Separator()
+                {
+                    Width = 1,
+                    [Grid.RowProperty] = 0,
+                    [Grid.RowSpanProperty] = 3,
+                    [Grid.ColumnProperty] = columnIndex
+                };
+                _rootChildren.Add(verticalSeparator);
+
+                // Generate Root GridSplitter's
+                var verticalGridSplitter = new GridSplitter()
+                {
+                    Width = 1,
+                    [Grid.RowProperty] = 0,
+                    [Grid.RowSpanProperty] = 3,
+                    [Grid.ColumnProperty] = columnIndex,
+                    [!GridSplitter.IsEnabledProperty] = this[!DataGrid.CanUserResizeColumnsProperty]
+                };
+                _rootChildren.Add(verticalGridSplitter);
+            }
+
+            foreach (var child in _rootChildren)
+            {
+                _root.Children.Add(child);
+            }
+        }
+
+        private void InvalidateColumnHeaders()
+        {
+            if (_columnHeaders is null)
+            {
+                return;
+            }
+
+            foreach (var child in _columnHeadersChildren)
+            {
+                _columnHeaders.Children.Remove(child);
+            }
+
+            var columns = Columns;
+ 
+            // Generate ColumnHeaders ColumnDefinitions
+
+            var columnDefinitions = new List<ColumnDefinition>();
+
+            for (var i = 0; i < columns.Count; i++)
+            {
+                var column = columns[i];
+
+                var columnDefinition = new ColumnDefinition()
+                {
+                    [!ColumnDefinition.WidthProperty] = column[!DataGridColumn.WidthProperty]
+                };
+                columnDefinitions.Add(columnDefinition);
+
+                // Generate ColumnHeaders DataGridColumnHeader's
+                var columnHeader = new DataGridColumnHeader()
+                {
+                    [Grid.ColumnProperty] = columnDefinitions.Count - 1,
+                    [!DataGridColumnHeader.HeaderProperty] = column[!DataGridColumn.HeaderProperty]
+                };
+                _columnHeadersChildren.Add(columnHeader);
+ 
+                if (i < columns.Count - 1)
+                {
+                    columnDefinitions.Add(new ColumnDefinition(new GridLength(1, GridUnitType.Pixel)));
+                }
+            }
+
+            _columnHeaders.SetColumnDefinitions(columnDefinitions);
+
+            _columnHeaders.SetValue(Grid.RowProperty, 0);
+            _columnHeaders.SetValue(Grid.ColumnProperty, 0);
+            _columnHeaders.SetValue(Grid.ColumnSpanProperty,  columns.Count + (columns.Count - 1));
+
+            foreach (var child in _columnHeadersChildren)
+            {
+                _columnHeaders.Children.Add(child);
+            }
+        }
+
+        private void InvalidateScrollViewer()
+        {
+            if (_scrollViewer is null)
+            {
+                return;
+            }
+ 
+            var columns = Columns;
+
+            _scrollViewer.SetValue(Grid.RowProperty, 2);
+            _scrollViewer.SetValue(Grid.ColumnProperty, 0);
+            _scrollViewer.SetValue(Grid.ColumnSpanProperty,  columns.Count + (columns.Count - 1));
         }
     }
 }
--- src/ItemsRepeaterDataGrid/DataGridRow.axaml.cs
+++ src/ItemsRepeaterDataGrid/DataGridRow.axaml.cs
@@ -1,12 +1,100 @@
-﻿using Avalonia.Controls.Primitives;
+﻿using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 
 namespace ItemsRepeaterDataGrid
 {
     public class DataGridRow : TemplatedControl
     {
+        private Grid? _root;
+        private List<Control> _rootChildren = new List<Control>();
+
+        public static readonly StyledProperty<AvaloniaList<DataGridColumn>> ColumnsProperty = 
+            AvaloniaProperty.Register<DataGridRow, AvaloniaList<DataGridColumn>>(nameof(Columns), new AvaloniaList<DataGridColumn>());
+
+        public AvaloniaList<DataGridColumn> Columns
+        {
+            get => GetValue(ColumnsProperty);
+            set => SetValue(ColumnsProperty, value);
+        }
+
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
         {
             base.OnApplyTemplate(e);
+
+            _root = e.NameScope.Find<Grid>("PART_Root");
+
+            InvalidateRoot();
+        }
+
+        protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+        {
+            base.OnPropertyChanged(change);
+
+            if (change.Property == ColumnsProperty)
+            {
+                InvalidateRoot();
+            }
+        }
+
+        private void InvalidateRoot()
+        {
+            if (_root is null)
+            {
+                return;
+            }
+
+            foreach (var child in _rootChildren)
+            {
+                _root.Children.Remove(child);
+            }
+
+            var columns = Columns;
+ 
+            // Generate Root ColumnDefinitions
+
+            var columnDefinitions = new List<ColumnDefinition>();
+
+            for (var i = 0; i < columns.Count; i++)
+            {
+                var column = columns[i];
+
+                var columnDefinition = new ColumnDefinition()
+                {
+                    [!ColumnDefinition.WidthProperty] = column[!DataGridColumn.WidthProperty]
+                };
+                columnDefinitions.Add(columnDefinition);
+
+                // Generate DataGridCell's
+
+                if (column is DataGridTemplateColumn templateColumn)
+                {
+                    var cell = new DataGridCell()
+                    {
+                        [Grid.ColumnProperty] = columnDefinitions.Count - 1,
+                        [!DataGridCell.CellTemplateProperty] = templateColumn[!DataGridTemplateColumn.CellTemplateProperty]
+                    };
+                    _rootChildren.Add(cell);
+                }
+                else
+                {
+                    // TODO: Add support of other column types.
+                }
+
+                if (i < columns.Count - 1)
+                {
+                    columnDefinitions.Add(new ColumnDefinition(new GridLength(1, GridUnitType.Pixel)));
+                }
+            }
+
+            _root.SetColumnDefinitions(columnDefinitions);
+
+            foreach (var child in _rootChildren)
+            {
+                _root.Children.Add(child);
+            }
         }
     }
 }
