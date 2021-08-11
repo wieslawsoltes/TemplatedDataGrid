fb3b6ec9d95eaff5cb1c9e05be6138d50194436f
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-08-11 09:31:50 +0200

    Create GridExtensions.cs

---
+++ src/ItemsRepeaterDataGrid/GridExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Avalonia.Controls;
+
+namespace ItemsRepeaterDataGrid
+{
+    public static class GridExtensions
+    {
+        public static void SetRowDefinitions(this Grid grid, IEnumerable<RowDefinition> rowDefinitions)
+        {
+            grid.RowDefinitions.Clear();
+            grid.RowDefinitions.AddRange(rowDefinitions);
+        }
+
+        public static void SetColumnDefinitions(this Grid grid, IEnumerable<ColumnDefinition> columnDefinitions)
+        {
+            grid.ColumnDefinitions.Clear();
+            grid.ColumnDefinitions.AddRange(columnDefinitions);
+        }
+    }
+}
