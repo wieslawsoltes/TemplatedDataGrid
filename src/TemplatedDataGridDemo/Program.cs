99bd7108d70bffe58921b0d8163576599ac594df
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-08-12 10:07:00 +0200

    Update Program.cs

--- src/TemplatedDataGridDemo/Program.cs
+++ src/TemplatedDataGridDemo/Program.cs
@@ -16,10 +16,21 @@ namespace TemplatedDataGridDemo
         }
 
         public static AppBuilder BuildAvaloniaApp()
-
-        { 
+        {
             return AppBuilder.Configure<App>()
                 .UsePlatformDetect()
+                .With(new Win32PlatformOptions()
+                {
+                    UseDeferredRendering = true
+                })
+                .With(new X11PlatformOptions()
+                {
+                    UseDeferredRendering = true
+                })
+                .With(new AvaloniaNativePlatformOptions()
+                {
+                    UseDeferredRendering = true
+                })
                 .LogToTrace()
                 .UseReactiveUI();
         }
