5130a7307611cb27d902d3d9378c661706da5041
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-04 14:56:22 +0200

    Create TemplatedDataGridDetailsPresenter.cs

---
+++ src/TemplatedDataGrid/Primitives/TemplatedDataGridDetailsPresenter.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls.Primitives;
+
+namespace TemplatedDataGrid.Primitives
+{
+    public class TemplatedDataGridDetailsPresenter : TemplatedControl
+    {
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);
+            
+            // TODO: Implement DataGridDetailsPresenter
+        }
+    }
+}
