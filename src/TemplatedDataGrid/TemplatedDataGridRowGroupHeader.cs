d35522aa3fe850fa59726feddbc37994e0bb130b
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-04 14:56:41 +0200

    Create TemplatedDataGridRowGroupHeader.cs

---
+++ src/TemplatedDataGrid/TemplatedDataGridRowGroupHeader.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls.Primitives;
+
+namespace TemplatedDataGrid
+{
+    public class TemplatedDataGridRowGroupHeader : TemplatedControl
+    {
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);
+
+            // TODO: Implement TemplatedDataGridRowGroupHeader
+        }
+    }
+}
