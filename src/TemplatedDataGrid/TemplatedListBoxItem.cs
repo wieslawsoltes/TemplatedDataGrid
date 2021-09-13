ffffc6ea22349d05dea9495a8629a62b9cef0d23
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-13 18:01:29 +0200

    Add TemplatedListBox

---
+++ src/TemplatedDataGrid/TemplatedListBoxItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Styling;
+
+namespace TemplatedDataGrid
+{
+    public class TemplatedListBoxItem : ListBoxItem, IStyleable
+    {
+        Type IStyleable.StyleKey => typeof(ListBoxItem);
+
+        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnAttachedToVisualTree(e);
+#if DEBUG
+            Console.WriteLine($"[TemplatedListBoxItem.Attached] {DataContext}");
+#endif
+        }
+
+        protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnDetachedFromVisualTree(e);
+#if DEBUG
+            Console.WriteLine($"[TemplatedListBoxItem.Detach] {DataContext}");
+#endif
+        }
+    }
+}
