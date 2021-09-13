c9f7cf8b7420160f7b79e28f25d06fe29eb5821e
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-13 12:04:05 +0200

    Add BindingExtensions class

---
+++ src/TemplatedDataGrid/BindingExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reactive.Disposables;
+using Avalonia;
+using Avalonia.Data;
+
+namespace TemplatedDataGrid
+{
+    internal static class BindingExtensions
+    {
+        public static IDisposable DisposeWith(this IDisposable item, CompositeDisposable compositeDisposable)
+        {
+            if (compositeDisposable is null)
+            {
+                throw new ArgumentNullException(nameof(compositeDisposable));
+            }
+
+            compositeDisposable.Add(item);
+            return item;
+        }
+
+        public static IDisposable OneWayBind(this IAvaloniaObject target, AvaloniaProperty targetProperty, IAvaloniaObject source, AvaloniaProperty sourceProperty, CompositeDisposable? compositeDisposable = null)
+        {
+            var disposable = target.Bind(targetProperty, new Binding(sourceProperty.Name, BindingMode.OneWay) { Source = source });
+            if (compositeDisposable is { })
+            {
+                compositeDisposable.Add(compositeDisposable);
+            }
+            return disposable;
+        }
+
+        public static IDisposable TwoWayBind(this IAvaloniaObject target, AvaloniaProperty targetProperty, IAvaloniaObject source, AvaloniaProperty sourceProperty, CompositeDisposable? compositeDisposable = null)
+        {
+            var disposable = target.Bind(targetProperty, new Binding(sourceProperty.Name, BindingMode.TwoWay) { Source = source });
+            if (compositeDisposable is { })
+            {
+                compositeDisposable.Add(compositeDisposable);
+            }
+            return disposable;
+        }
+        
+        public static IDisposable OneWayBind<T>(this IAvaloniaObject target, AvaloniaProperty targetProperty, IObservable<T> observable, CompositeDisposable? compositeDisposable = null)
+        {
+            var disposable = target.Bind(targetProperty, observable.ToBinding());
+
+            if (compositeDisposable is { })
+            {
+                compositeDisposable.Add(compositeDisposable);
+            }
+            return disposable;
+        }
+    }
+}
